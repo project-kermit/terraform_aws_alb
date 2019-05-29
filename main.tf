4b69547ff4faea26c56767a94ae87cf58e03ce66
Mark Woodhall <mark.woodhall@gmail.com>
2019-05-29 11:12:05 +0100

    Terraform 0.12 upgrade

--- main.tf
+++ main.tf
@@ -36,4 +36,3 @@
  *       project_path        = "your/project"
  *     }
  */
-
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 
 // The path of the project in VCS.
 variable "project_path" {
-  type    = "string"
+  type    = string
   default = ""
 }
 
@@ -10,48 +10,49 @@ variable "project_path" {
 // elastic IP assigned to it. The default is "false", which gives the ELB an
 // elastic (public) IP.
 variable "internal_alb" {
-  type    = "string"
+  type    = string
   default = "false"
 }
 
 // The listener subnet IDs.
 variable "listener_subnet_ids" {
-  type = "list"
+  type = list(string)
 }
 
 // The external port that the ALB will listen to requests on.
 variable "listener_port" {
-  type    = "string"
+  type    = string
   default = "80"
 }
 
 // The listener protocol. Can be one of HTTP or HTTPS.
 variable "listener_protocol" {
-  type    = "string"
+  type    = string
   default = "HTTP"
 }
 
 // Restrict access to specified networks, supplied as list.
 variable "restrict_to_networks" {
-  type    = "list"
+  type    = list(string)
   default = ["0.0.0.0/0"]
 }
 
 // The ARN of the server certificate you want to use with the listener.
 // Required for HTTPS listeners.
 variable "listener_certificate_arn" {
-  type    = "string"
+  type    = string
   default = ""
 }
 
 // The port that the default target group will pass requests to.
 variable "default_target_group_port" {
-  type    = "string"
+  type    = string
   default = "80"
 }
 
 // The protocol for the default target group.
 variable "default_target_group_protocol" {
-  type    = "string"
+  type    = string
   default = "HTTP"
 }
+
